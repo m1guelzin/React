2f48b8288cf149fec1c4b6df6d27be7289c895a5
m1guelzin <miguelgarridodev@gmail.com>
2025-01-29 11:26:27 -0300

    Exemplos aula

---
+++ aulaReact/src/App.jsx
@@ -0,0 +1,11 @@
+import Component1 from "./Component";
+
+function App() {
+  return (
+    <div>
+      <Component1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ aulaReact/src/Component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Component1(){
+    return(
+        <div>
+        <h1 style={{padding:'10px 20px', color:'blue'}}>Componente que pode ser reutilizado</h1>
+        </div>
+    )
+}
+
+export default Component1
