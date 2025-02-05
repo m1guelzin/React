fc312a228afc4d5b0ca419dda6e4708ef83348d1
m1guelzin <miguelgarridodev@gmail.com>
2025-02-05 13:49:01 -0300

    formulario de login

---
+++ telaLogin/src/App.jsx
@@ -0,0 +1,11 @@
+import Login from './Login/Login.jsx'
+
+function App() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  )
+}
+
+export default App
---
+++ telaLogin/src/Login/Login.jsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
+function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Email: " +user.email + " " + "Senha: "+user.password)
+  };
+
+  return (
+    <Container component="main" maxWidth="xl">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          sx={{
+            margin: 1,
+            backgroundColor: "green",
+          }}
+        >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Vio
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            mt: 1,
+          }}
+          onSubmit={handleSubmit}
+          noValidate
+        >
+          <TextField
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            margin="normal"
+            value={user.email}
+            onChange={onChange}
+          />
+          <TextField
+            required
+            fullWidth
+            id="password"
+            label="Senha"
+            name="password"
+            margin="normal"
+            type="password"
+            value={user.password}
+            onChange={onChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: "green",
+            }}
+          >
+            Entrar
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+export default Login;
