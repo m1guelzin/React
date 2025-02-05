8118c57aa528a2d8844516d9c28cc2ec62f24a6c
m1guelzin <miguelgarridodev@gmail.com>
2025-02-05 13:48:27 -0300

    Formulariode Cadastro/

---
+++ cadastroForm/src/App.jsx
@@ -0,0 +1,11 @@
+import Cadastro from './Cadastro/Cadastro'
+
+function App() {
+  return (
+    <div>
+      <Cadastro></Cadastro>
+    </div>
+  )
+}
+
+export default App
---
+++ cadastroForm/src/Cadastro/Cadastro.jsx
@@ -0,0 +1,130 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
+function Cadastro() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    age: "",
+    name: "",
+  });
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(
+      "Usuário cadastrado com Sucesso!" + " " +
+        "Email: " +
+        user.email +
+        " " +
+        "Senha: " +
+        user.password +
+        " " +
+        "Idade: " +
+        user.age +
+        " " +
+        "Nome: " +
+        user.name
+    );
+  };
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          sx={{
+            margin: 1,
+            backgroundColor: "#00A8FF",
+          }}
+        >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Cadastro
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            mt: 1,
+          }}
+          onSubmit={handleSubmit}
+          noValidate
+        >
+          <TextField
+            required
+            fullWidth
+            id="name"
+            label="Nome"
+            name="name"
+            margin="normal"
+            value={user.name}
+            onChange={onChange}
+          />
+          <TextField
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            margin="normal"
+            value={user.email}
+            onChange={onChange}
+          />
+          <TextField
+            required
+            fullWidth
+            id="age"
+            label="Idade"
+            name="age"
+            margin="normal"
+            type="number"
+            value={user.age}
+            onChange={onChange}
+          />
+          <TextField
+            required
+            fullWidth
+            id="password"
+            label="Senha"
+            name="password"
+            margin="normal"
+            type="password"
+            value={user.password}
+            onChange={onChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: "#00A8FF",
+            }}
+          >
+            Entrar
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+export default Cadastro;
